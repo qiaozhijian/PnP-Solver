5f0f51c7f5c2a5d383cc0ba7f4f925132977245f
qiaozhijian <935514540@qq.com>
2022-01-13 21:09:09 +0800

    update

---
+++ include/System.h
@@ -0,0 +1,18 @@
+//
+// Created by qzj on 2021/12/24.
+//
+
+#ifndef CMAKE_TEMPLATE_SYSTEM_H
+#define CMAKE_TEMPLATE_SYSTEM_H
+
+
+class System {
+
+    public:
+
+    System();
+
+};
+
+
+#endif //CMAKE_TEMPLATE_SYSTEM_H
